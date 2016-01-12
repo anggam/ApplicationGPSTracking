88ff792b2cf516d9c84c6c64929528e189696f43
mrdistiara <mrdistiara@gmail.com>
2016-01-12 13:07:42 +0700

    masih ada yang error

--- app/src/main/java/com/example/runrun/applicationgpstracking/MenuActivity.java
+++ app/src/main/java/com/example/runrun/applicationgpstracking/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.runrun.applicationgpstracking;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +13,12 @@ import android.widget.ProgressBar;
 import com.example.runrun.applicationgpstracking.adapters.FriendsAdapter;
 import com.example.runrun.applicationgpstracking.helpers.HttpHelper;
 import com.example.runrun.applicationgpstracking.model.User;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
@@ -22,7 +29,7 @@ import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends FragmentActivity implements OnMapReadyCallback {
     private Toolbar actionBarToolbar;
     private FriendsAdapter friendsAdapter;
     private ListView friendsLV;
@@ -33,14 +40,21 @@ public class MenuActivity extends AppCompatActivity {
     private View friendPage;
     private View mapPage;
 
+    private GoogleMap mMaps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
         initViews();
         initActionbar();
         initListView();
         getFriends();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
     }
 
     private void initViews() {
@@ -51,6 +65,25 @@ public class MenuActivity extends AppCompatActivity {
         friendsPB = (ProgressBar) findViewById(R.id.friend_list_pb);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerMenu = findViewById(R.id.left_drawer);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMaps = googleMap;
+        // Add a marker in Indoneisa and move the camera
+        LatLng indonesia = new LatLng(-6.914744, 107.609810);
+        mMaps.addMarker(new MarkerOptions().position(indonesia).title("Marker in Indonesia"));
+        mMaps.moveCamera(CameraUpdateFactory.newLatLng(indonesia));
     }
 
     private void getFriends() {
